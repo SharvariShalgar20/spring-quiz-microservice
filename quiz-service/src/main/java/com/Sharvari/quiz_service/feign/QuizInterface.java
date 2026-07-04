0e77e50b9f39106ca013d6f9eea33cd3351c6f4f
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-07-04 10:41:45 +0530

    feat : added 2 controller in QuizInterface in quiz-service and added 2 dto - QuestionWrapper, Response

--- quiz-service/src/main/java/com/Sharvari/quiz_service/feign/QuizInterface.java
+++ quiz-service/src/main/java/com/Sharvari/quiz_service/feign/QuizInterface.java
@@ -3,6 +3,8 @@ package com.Sharvari.quiz_service.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -11,4 +13,10 @@ public interface QuizInterface {
 
     @GetMapping("question/generate/{category}/{numQuestion}")
     List<Integer> getQuestionsForQuiz(@PathVariable String category, @PathVariable int numQuestion);
+
+    @PostMapping("question/getQuestions")
+    List<QuestionWrapper> getQuestionsFromId(@RequestBody List<Integer> ids);
+
+    @PostMapping("question/getScore")
+    Integer getScore(@RequestBody List<Response> responses);
 }
