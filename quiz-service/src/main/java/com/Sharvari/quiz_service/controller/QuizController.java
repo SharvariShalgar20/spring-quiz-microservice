3916731d712d26e11528ddf2c1977d33ab73b7c8
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-07-08 19:34:05 +0530

    add logging to QuizController

--- quiz-service/src/main/java/com/Sharvari/quiz_service/controller/QuizController.java
+++ quiz-service/src/main/java/com/Sharvari/quiz_service/controller/QuizController.java
@@ -3,11 +3,13 @@ package com.Sharvari.quiz_service.controller;
 import com.Sharvari.quiz_service.dto.QuestionWrapper;
 import com.Sharvari.quiz_service.dto.Response;
 import com.Sharvari.quiz_service.service.QuizService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
@@ -20,16 +22,19 @@ public class QuizController {
             @PathVariable String category,
             @PathVariable int numQuestion,
             @PathVariable String title) {
+        log.info("Quiz created.");
         return quizService.createQuiz(category, numQuestion, title);
     }
 
     @GetMapping("get/{quizId}")
     public List<QuestionWrapper> getQuizQuestions(@PathVariable Integer quizId) {
+        log.info("Fetching questions for quiz id: {}", quizId);
         return quizService.getQuizQuestions(quizId);
     }
 
     @PostMapping("submit/{quizId}")
     public Integer submitQuiz(@PathVariable Integer quizId, @RequestBody List<Response> responses) {
+        log.info("Submitting {} answers for quiz id: {}", responses.size(), quizId);
         return quizService.calculateResult(quizId, responses);
     }
 }
