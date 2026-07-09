135e6cf5612b973f74303f9c08c432c42a921f0c
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-07-09 20:00:46 +0530

    feat : User and UserRepository created to store user data

---
+++ auth-service/src/main/java/com/sharvari/auth_service/model/User.java
@@ -0,0 +1,25 @@
+package com.sharvari.auth_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+}
