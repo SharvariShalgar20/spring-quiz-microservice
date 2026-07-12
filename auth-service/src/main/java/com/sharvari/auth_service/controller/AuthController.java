bb35bcd415ba846b450149f7e8181261e1a9e154
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-07-12 22:37:35 +0530

    feat : AuthController created with 2 endpoints - Register, Login

---
+++ auth-service/src/main/java/com/sharvari/auth_service/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.sharvari.auth_service.controller;
+
+import com.sharvari.auth_service.dto.AuthResponse;
+import com.sharvari.auth_service.dto.LoginRequest;
+import com.sharvari.auth_service.dto.RegisterRequest;
+import com.sharvari.auth_service.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
---
+++ auth-service/src/main/java/com/sharvari/auth_service/service/AuthService.java
@@ -0,0 +1,20 @@
+package com.sharvari.auth_service.service;
+
+import com.sharvari.auth_service.dto.AuthResponse;
+import com.sharvari.auth_service.dto.LoginRequest;
+import com.sharvari.auth_service.dto.RegisterRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AuthService {
+
+    public AuthResponse register(RegisterRequest request) {
+        return null;
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        return null;
+    }
+}
