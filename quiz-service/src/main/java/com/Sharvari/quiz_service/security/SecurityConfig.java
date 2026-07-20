493af549d400e29a2ed50a8a632a124c2dd5f6e3
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-07-20 23:23:00 +0530

    feat : Update quiz-service's SecurityConfig with role checks

--- quiz-service/src/main/java/com/Sharvari/quiz_service/security/SecurityConfig.java
+++ quiz-service/src/main/java/com/Sharvari/quiz_service/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.Sharvari.quiz_service.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +12,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
@@ -24,7 +26,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/quiz/get/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/quiz/create", "/quiz/submit/**").authenticated()
+                        .requestMatchers("/quiz/create", "/quiz/submit/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
